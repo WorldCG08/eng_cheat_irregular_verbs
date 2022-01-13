3cb5589f4e98842a1c27853bdcbf03458b54bd07
ymelnyk <yurii.melnyk@dpgmm.net>
2022-01-13 13:41:53 +0200

    Add project files.

---
+++ eng_cheat/Model.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity.Infrastructure;
+
+namespace eng_cheat
+{
+    public class VerbContext : DbContext
+    {
+        public DbSet<Verb> Verbs { get; set; }
+    }
+
+    public class Verb
+    {
+        public int VerbId { get; set; }
+        public string Infinitive { get; set; }
+        public string Simple { get; set; }
+        public string Participle { get; set; }
+    }
+}
---
+++ eng_cheat/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace eng_cheat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new VerbContext())
+            {
+                string searchText = null;
+
+                while (searchText != "exit")
+                {
+                    Console.WriteLine("Search or type exit:");
+                    searchText = Console.ReadLine();
+                    Console.Clear();
+                    // Display all Blogs from the database
+                    var query = db.Verbs.Where(v => v.Infinitive.Contains(searchText) || v.Participle.Contains(searchText) || v.Simple.Contains(searchText));
+
+                    foreach (var item in query)
+                    {
+                        Console.WriteLine(item.Infinitive + "\t" + item.Simple + "\t" + item.Participle);
+                    }
+                }
+            }
+        }
+    }
+}
